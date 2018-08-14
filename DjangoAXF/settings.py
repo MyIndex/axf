f005ed725bd2768dbb24569d1498aee2cf0c1edd
MyIndex <964351153@qq.com>
2018-08-14 22:04:38 +0800

    对项目进行nginx和uwsgi的配置，完善部分项目

--- DjangoAXF/settings.py
+++ DjangoAXF/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4b#_@a$53+3ug0g$e&joux-o&*k9+#ph4q&wm0ftlufa9c&n%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
